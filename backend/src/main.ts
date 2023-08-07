3bfe1c55e57a23a670a43ab3d10ef980c0527536
FathirTHX <fathirisgaming12@gmail.com>
2023-08-07 13:58:15 +0700

    ..

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,15 +3,10 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-<<<<<<< HEAD
-  app.enableCors();
-  await app.listen(process.env.PORT,'192.168.18.210',()=>{
-=======
   app.enableCors({
     origin: "*"
   });
   await app.listen(process.env.PORT,'192.168.56.1',()=>{
->>>>>>> 8a812454e79a8a4fa91f59a80b825cef33e5e24d
     console.log('successfull connect')
   })
 }
